1db3723eb61f09d0927c5059c34a080d07c89d1d
zac Gerber <Zgerber91@gmail.com>
2020-10-02 12:31:47 -0400

    initial commit

--- src/App.js
+++ src/App.js
@@ -2,25 +2,45 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  constructor(props){
+    super(props);
+    this.state = {
+      roastboasts:[],
+    };
+  }
+
+
+componentDidMount() {
+  fetch("http://127.0.0.1:8000/api/roastboast/")
+  .then(res => res.json())
+  .then(data => this.setState ({roastboasts:data}));
+}
+
+
+  render() {
+    return (
+      <div>
+        <ul>
+          {this.state.roastboasts.map(p => (
+            <div>
+              <ul>
+                <li>
+                  <h3>Message:</h3>{p.body},
+                  <h3>Upvote:</h3>{p.upvote},
+                  <h3>DownVote:</h3>{p.downvote},
+                  <h3>Total Votes:</h3>{p.total_vote},
+                  <h3>This is:</h3>{p.choices = this.state.choices === true ? 'posts': 'Boast'},
+                  <h3>Posted on:</h3>{p.post_date}
+                </li>
+              </ul>
+            </div>
+          ))}
+        </ul>
+      </div>
+    )
+
+  };
 }
 
 export default App;
